5d423efea1738311a8953ef5d0aee63feacfdfd3
Karel <karel.hb@email.cz>
2021-12-25 23:51:53 +0100

    Oprava WorldViewModel.

--- app/src/main/java/com/example/worldroom/WordViewModel.kt
+++ app/src/main/java/com/example/worldroom/WordViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import androidx.lifecycle.asLiveData
 
 class WordViewModel(private val repository: WordRepository) : ViewModel() {
 
